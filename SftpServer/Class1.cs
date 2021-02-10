077820b27cfb54a7a02b99bcdf1be08636240a2e
MARCO ANTONIO CONEJO VARGAS <marcoconejo@outlook.com>
2021-02-09 21:40:41 -0600

    Added Initial Sftp Support

---
+++ SftpServer/Class1.cs
@@ -0,0 +1,22 @@
+﻿using Renci.SshNet;
+using System;
+
+namespace SftpServer
+{
+    public class Class1
+    {
+        public SftpInfo sftpInfo { get; set; }
+        public Class1()
+        {
+       
+        }
+
+        public void Main(string [] args)
+        {
+            using (SftpClient client = new SftpClient(new PrivateKeyConnectionInfo(sftpInfo.Hostname, sftpInfo.Username, sftpInfo.Keyfile)))
+            {
+
+            };
+        }
+    }
+}
---
+++ SftpServer/SftpInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Renci.SshNet
+
+namespace SftpServer
+{
+    public class SftpInfo
+    {
+        public string Username { get; set; }
+        public string Hostname { get; set; }
+        public PrivateKeyFile Keyfile { get; set; }
+    }
+}
